c54f573053f1c761ee7ce243eabbc7a2389b13a4
B230ft <igorniyazov@gmail.com>
2019-02-27 21:04:13 -0800

    Updates

---
+++ precache-manifest.bc4aafa90e0c6e17a3e65e03cd04dea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/B230ft/clicky-game-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0963918368e7678222e8",
+    "url": "/B230ft/clicky-game-react/static/js/runtime~main.09639183.js"
+  },
+  {
+    "revision": "3221f6f851ddb6b5923e",
+    "url": "/B230ft/clicky-game-react/static/js/main.3221f6f8.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/B230ft/clicky-game-react/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "3221f6f851ddb6b5923e",
+    "url": "/B230ft/clicky-game-react/static/css/main.676dfed5.chunk.css"
+  },
+  {
+    "revision": "5b507d00c0184260c758749272f17872",
+    "url": "/B230ft/clicky-game-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/B230ft/clicky-game-react/precache-manifest.bc4aafa90e0c6e17a3e65e03cd04dea2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/B230ft/clicky-game-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
